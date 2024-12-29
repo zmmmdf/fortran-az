75ea3a139dc7ff9bc8a7c17abe684f4b2ac9c6b4
Ziya <151031540+zmmmdf@users.noreply.github.com>
2024-12-29 23:56:11 +0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,63 @@
+
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  themeSwitch: {
+      useOptions() {
+        return {
+          light: 'Light',
+          dark: 'Dark',
+          system: 'System'
+        }
+      }
+    }
+  },
+  toc: {
+    title: "Bu səhifədə"
+  },
+  search: {
+    placeholder: "Səhifədə axtar..."
+  },
+  navigation: true,
+  gitTimestamp: 'Son dəfə Dekabr 2024  tarixində redaktə edilib',
+  feedback: {
+    content: null
+  },
+  editLink: {
+    component: null
+  },
+  logo: <span>fortran/az</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/zmmmdf/fortran-az',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://youtube.com/@zmmmdf',
+    icon: (
+      <>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg" style={{height:"25px"}}/>
+      </>
+    )
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/zmmmdf/fortran-az',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+        {new Date().getFullYear()}❤️{' '}
+        <a href="https://nextra.site" target="_blank">
+          Ziya M.
+        </a>
+      </span>
+    )
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: 'Fortran Dakumentasiya | Ziya Mammadov'
+      }
+    }
   },
 }
 
